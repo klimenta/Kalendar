3c76af21354be24f33478c3d2b978e7df01051b5
Kliment Andreev <klimenta@iandreev.com>
2021-01-14 09:00:49 -0500

    Final release v0.0.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace Kalendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.mcCalendar = new System.Windows.Forms.MonthCalendar();
+            this.cbMonth = new System.Windows.Forms.ComboBox();
+            this.cbYear = new System.Windows.Forms.ComboBox();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.btnDiff = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mcCalendar
+            // 
+            this.mcCalendar.Location = new System.Drawing.Point(10, 9);
+            this.mcCalendar.Margin = new System.Windows.Forms.Padding(14);
+            this.mcCalendar.Name = "mcCalendar";
+            this.mcCalendar.ShowWeekNumbers = true;
+            this.mcCalendar.TabIndex = 0;
+            // 
+            // cbMonth
+            // 
+            this.cbMonth.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbMonth.FormattingEnabled = true;
+            this.cbMonth.Items.AddRange(new object[] {
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"});
+            this.cbMonth.Location = new System.Drawing.Point(10, 265);
+            this.cbMonth.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cbMonth.Name = "cbMonth";
+            this.cbMonth.Size = new System.Drawing.Size(170, 28);
+            this.cbMonth.TabIndex = 3;
+            this.cbMonth.SelectedIndexChanged += new System.EventHandler(this.cbMonth_SelectedIndexChanged);
+            // 
+            // cbYear
+            // 
+            this.cbYear.FormattingEnabled = true;
+            this.cbYear.Location = new System.Drawing.Point(182, 265);
+            this.cbYear.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cbYear.Name = "cbYear";
+            this.cbYear.Size = new System.Drawing.Size(170, 28);
+            this.cbYear.TabIndex = 4;
+            this.cbYear.SelectedIndexChanged += new System.EventHandler(this.cbYear_SelectedIndexChanged);
+            this.cbYear.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cbYear_KeyPress);
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Location = new System.Drawing.Point(13, 298);
+            this.lblVersion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(244, 20);
+            this.lblVersion.TabIndex = 5;
+            this.lblVersion.Text = "Kalendar v0.0.1 - K.Andreev 2021";
+            // 
+            // btnDiff
+            // 
+            this.btnDiff.BackColor = System.Drawing.Color.Yellow;
+            this.btnDiff.Location = new System.Drawing.Point(327, 296);
+            this.btnDiff.Name = "btnDiff";
+            this.btnDiff.Size = new System.Drawing.Size(25, 25);
+            this.btnDiff.TabIndex = 6;
+            this.btnDiff.Text = ">";
+            this.btnDiff.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 329);
+            this.Controls.Add(this.btnDiff);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.cbYear);
+            this.Controls.Add(this.cbMonth);
+            this.Controls.Add(this.mcCalendar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Kalendar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar mcCalendar;
+        private System.Windows.Forms.ComboBox cbMonth;
+        private System.Windows.Forms.ComboBox cbYear;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Button btnDiff;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalendar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mcCalendar.ShowWeekNumbers = true;
+            int intMonth = DateTime.Now.Month;
+            cbMonth.SelectedIndex = intMonth - 1;
+            for (int x = 1999; x < 2030; x++)
+            {
+                cbYear.Items.Add(x.ToString());
+            }
+        }
+
+        private void cbMonth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CalculateDate();
+        }
+
+        private void CalculateDate()
+        {
+            DateTime today = mcCalendar.TodayDate;
+            int year;
+            try
+            {
+                year = int.Parse(cbYear.Text);
+            }
+            catch
+            {
+                year = today.Year;
+            }
+            
+            try
+            {
+                DateTime selStart = new DateTime(year, cbMonth.SelectedIndex + 1, 1);
+                DateTime selEnd = selStart.AddDays(0);
+                mcCalendar.SelectionStart = selStart.AddDays(0);
+                mcCalendar.SelectionEnd = selEnd.AddDays(0);
+            }
+            catch
+            {
+                year = today.Year;
+            }
+        }
+
+        private void cbYear_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CalculateDate();
+        }
+
+        private void cbYear_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) CalculateDate();
+        }
+    }
+}
