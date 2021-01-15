3b956652b27ab73cd5bf499f1d3f9d3a604243f0
Kliment Andreev <klimenta@iandreev.com>
2021-01-15 12:00:19 -0500

    v0.0.1 Final version

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,20 +30,16 @@ namespace Kalendar
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.mcCalendar = new System.Windows.Forms.MonthCalendar();
             this.cbMonth = new System.Windows.Forms.ComboBox();
             this.cbYear = new System.Windows.Forms.ComboBox();
             this.lblVersion = new System.Windows.Forms.Label();
-            this.btnDiff = new System.Windows.Forms.Button();
+            this.btnToggle = new System.Windows.Forms.Button();
+            this.mcCalendarR = new System.Windows.Forms.MonthCalendar();
+            this.lblDiff = new System.Windows.Forms.Label();
+            this.lblLeft = new System.Windows.Forms.Label();
+            this.lblRight = new System.Windows.Forms.Label();
+            this.mcCalendarL = new System.Windows.Forms.MonthCalendar();
             this.SuspendLayout();
-            // 
-            // mcCalendar
-            // 
-            this.mcCalendar.Location = new System.Drawing.Point(10, 9);
-            this.mcCalendar.Margin = new System.Windows.Forms.Padding(14);
-            this.mcCalendar.Name = "mcCalendar";
-            this.mcCalendar.ShowWeekNumbers = true;
-            this.mcCalendar.TabIndex = 0;
             // 
             // cbMonth
             // 
@@ -62,20 +58,18 @@ namespace Kalendar
             "October",
             "November",
             "December"});
-            this.cbMonth.Location = new System.Drawing.Point(10, 265);
-            this.cbMonth.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cbMonth.Location = new System.Drawing.Point(7, 172);
             this.cbMonth.Name = "cbMonth";
-            this.cbMonth.Size = new System.Drawing.Size(170, 28);
+            this.cbMonth.Size = new System.Drawing.Size(124, 21);
             this.cbMonth.TabIndex = 3;
             this.cbMonth.SelectedIndexChanged += new System.EventHandler(this.cbMonth_SelectedIndexChanged);
             // 
             // cbYear
             // 
             this.cbYear.FormattingEnabled = true;
-            this.cbYear.Location = new System.Drawing.Point(182, 265);
-            this.cbYear.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cbYear.Location = new System.Drawing.Point(137, 172);
             this.cbYear.Name = "cbYear";
-            this.cbYear.Size = new System.Drawing.Size(170, 28);
+            this.cbYear.Size = new System.Drawing.Size(119, 21);
             this.cbYear.TabIndex = 4;
             this.cbYear.SelectedIndexChanged += new System.EventHandler(this.cbYear_SelectedIndexChanged);
             this.cbYear.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cbYear_KeyPress);
@@ -83,36 +77,85 @@ namespace Kalendar
             // lblVersion
             // 
             this.lblVersion.AutoSize = true;
-            this.lblVersion.Location = new System.Drawing.Point(13, 298);
-            this.lblVersion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblVersion.Location = new System.Drawing.Point(4, 209);
             this.lblVersion.Name = "lblVersion";
-            this.lblVersion.Size = new System.Drawing.Size(244, 20);
+            this.lblVersion.Size = new System.Drawing.Size(168, 13);
             this.lblVersion.TabIndex = 5;
             this.lblVersion.Text = "Kalendar v0.0.1 - K.Andreev 2021";
             // 
-            // btnDiff
+            // btnToggle
             // 
-            this.btnDiff.BackColor = System.Drawing.Color.Yellow;
-            this.btnDiff.Location = new System.Drawing.Point(327, 296);
-            this.btnDiff.Name = "btnDiff";
-            this.btnDiff.Size = new System.Drawing.Size(25, 25);
-            this.btnDiff.TabIndex = 6;
-            this.btnDiff.Text = ">";
-            this.btnDiff.UseVisualStyleBackColor = false;
+            this.btnToggle.BackColor = System.Drawing.Color.Yellow;
+            this.btnToggle.Location = new System.Drawing.Point(239, 198);
+            this.btnToggle.Margin = new System.Windows.Forms.Padding(2);
+            this.btnToggle.Name = "btnToggle";
+            this.btnToggle.Size = new System.Drawing.Size(18, 24);
+            this.btnToggle.TabIndex = 6;
+            this.btnToggle.Text = ">";
+            this.btnToggle.UseVisualStyleBackColor = false;
+            this.btnToggle.Click += new System.EventHandler(this.btnToggle_Click);
+            // 
+            // mcCalendarR
+            // 
+            this.mcCalendarR.Location = new System.Drawing.Point(271, 6);
+            this.mcCalendarR.MaxSelectionCount = 1;
+            this.mcCalendarR.Name = "mcCalendarR";
+            this.mcCalendarR.ShowWeekNumbers = true;
+            this.mcCalendarR.TabIndex = 7;
+            this.mcCalendarR.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.mcCalendarR_DateChanged);
+            // 
+            // lblDiff
+            // 
+            this.lblDiff.AutoSize = true;
+            this.lblDiff.Location = new System.Drawing.Point(268, 209);
+            this.lblDiff.Name = "lblDiff";
+            this.lblDiff.Size = new System.Drawing.Size(18, 13);
+            this.lblDiff.TabIndex = 8;
+            this.lblDiff.Text = "D:";
+            // 
+            // lblLeft
+            // 
+            this.lblLeft.AutoSize = true;
+            this.lblLeft.Location = new System.Drawing.Point(268, 172);
+            this.lblLeft.Name = "lblLeft";
+            this.lblLeft.Size = new System.Drawing.Size(16, 13);
+            this.lblLeft.TabIndex = 9;
+            this.lblLeft.Text = "L:";
+            // 
+            // lblRight
+            // 
+            this.lblRight.AutoSize = true;
+            this.lblRight.Location = new System.Drawing.Point(268, 190);
+            this.lblRight.Name = "lblRight";
+            this.lblRight.Size = new System.Drawing.Size(18, 13);
+            this.lblRight.TabIndex = 10;
+            this.lblRight.Text = "R:";
+            // 
+            // mcCalendarL
+            // 
+            this.mcCalendarL.Location = new System.Drawing.Point(7, 6);
+            this.mcCalendarL.MaxSelectionCount = 1;
+            this.mcCalendarL.Name = "mcCalendarL";
+            this.mcCalendarL.ShowWeekNumbers = true;
+            this.mcCalendarL.TabIndex = 11;
+            this.mcCalendarL.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.mcCalendarL_DateChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(358, 329);
-            this.Controls.Add(this.btnDiff);
+            this.ClientSize = new System.Drawing.Size(529, 233);
+            this.Controls.Add(this.mcCalendarL);
+            this.Controls.Add(this.lblRight);
+            this.Controls.Add(this.lblLeft);
+            this.Controls.Add(this.lblDiff);
+            this.Controls.Add(this.mcCalendarR);
+            this.Controls.Add(this.btnToggle);
             this.Controls.Add(this.lblVersion);
             this.Controls.Add(this.cbYear);
             this.Controls.Add(this.cbMonth);
-            this.Controls.Add(this.mcCalendar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Kalendar";
@@ -123,12 +166,15 @@ namespace Kalendar
         }
 
         #endregion
-
-        private System.Windows.Forms.MonthCalendar mcCalendar;
         private System.Windows.Forms.ComboBox cbMonth;
         private System.Windows.Forms.ComboBox cbYear;
         private System.Windows.Forms.Label lblVersion;
-        private System.Windows.Forms.Button btnDiff;
+        private System.Windows.Forms.Button btnToggle;
+        private System.Windows.Forms.MonthCalendar mcCalendarR;
+        private System.Windows.Forms.Label lblDiff;
+        private System.Windows.Forms.Label lblLeft;
+        private System.Windows.Forms.Label lblRight;
+        private System.Windows.Forms.MonthCalendar mcCalendarL;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,18 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Kalendar
 {
     public partial class Form1 : Form
     {
+
+        //Toggle used for the screen expand
+        bool bFormToggle = false;
+        
         public Form1()
         {
             InitializeComponent();
@@ -20,54 +16,101 @@ namespace Kalendar
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            mcCalendar.ShowWeekNumbers = true;
+            this.Width = 280;            
+            mcCalendarL.ShowWeekNumbers = true;
+            //Get the current month and show in combobox
             int intMonth = DateTime.Now.Month;
             cbMonth.SelectedIndex = intMonth - 1;
+            //Add years from 1999 to 2030 in combobox
             for (int x = 1999; x < 2030; x++)
             {
                 cbYear.Items.Add(x.ToString());
             }
+            //Show today's date in the labels
+            lblLeft.Text = "L: " + mcCalendarL.TodayDate.ToShortDateString();
+            lblRight.Text = "R: " + mcCalendarR.TodayDate.ToShortDateString();
         }
 
         private void cbMonth_SelectedIndexChanged(object sender, EventArgs e)
         {
-            CalculateDate();
+            ShowCalendar();
         }
 
-        private void CalculateDate()
+        //Shows the calendar on the left when month or year changes in the combos
+        private void ShowCalendar()
         {
-            DateTime today = mcCalendar.TodayDate;
+            DateTime dateLeft = mcCalendarL.SelectionRange.Start;
+            DateTime dateRight = mcCalendarR.SelectionRange.Start;
             int year;
+
             try
             {
                 year = int.Parse(cbYear.Text);
             }
             catch
             {
-                year = today.Year;
+                year = dateLeft.Year;
             }
             
             try
             {
                 DateTime selStart = new DateTime(year, cbMonth.SelectedIndex + 1, 1);
                 DateTime selEnd = selStart.AddDays(0);
-                mcCalendar.SelectionStart = selStart.AddDays(0);
-                mcCalendar.SelectionEnd = selEnd.AddDays(0);
+                mcCalendarL.SelectionStart = selStart.AddDays(0);
+                mcCalendarL.SelectionEnd = selEnd.AddDays(0);
             }
             catch
             {
-                year = today.Year;
+                year = dateLeft.Year;
             }
         }
 
         private void cbYear_SelectedIndexChanged(object sender, EventArgs e)
         {
-            CalculateDate();
+            ShowCalendar();
         }
 
+        //Change year when ENTER key is pressed
         private void cbYear_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (e.KeyChar == 13) CalculateDate();
+            if (e.KeyChar == 13) ShowCalendar();
+        }
+
+        //Expands/reduces the form when the yellow button is clicked
+        private void btnToggle_Click(object sender, EventArgs e)
+        {
+            if (bFormToggle)
+            {
+                btnToggle.Text = ">";
+                bFormToggle = false;
+                this.Width = 280;
+            }
+            else
+            {
+                btnToggle.Text = "<";
+                bFormToggle = true;
+                this.Width = 544;
+            }
+        }
+
+        private void mcCalendarR_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            CalculateDiff();
+        }
+
+        //Calculate the difference in days between two calendars
+        private void CalculateDiff()
+        {
+            DateTime dateLeft = mcCalendarL.SelectionRange.Start;
+            DateTime dateRight = mcCalendarR.SelectionRange.Start;
+            lblLeft.Text = "L: " + dateLeft.ToShortDateString();
+            lblRight.Text = "R: " + dateRight.ToShortDateString();
+            lblDiff.Text = "Diff: " + (dateRight - dateLeft).TotalDays.ToString();
+        }
+
+        private void mcCalendarL_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            CalculateDiff();
         }
     }
 }
